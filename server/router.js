e7d14b694976472946b7bd89a3ee6687fc3e7d03
Bryant Young <orb.lucid@gmail.com>
2018-01-11 00:29:16 -0800

    Finishing up wow api wrapper

--- server/router.js
+++ server/router.js
@@ -67,13 +67,122 @@ router.get('/api/overwatch/:tag?', function (req, res, next) {
 });
 router.get('/api/discord', function (req, res, next) {
     let url = 'https://discordapp.com/api/guilds/259097331046023168/widget.json';
-    request.get(url).end((err, response) => {
-        res.json(JSON.parse(response.text));
-    });
+    fetch(url, res);
 });
 
+// World of Warcraft API
+router.get('/api/wow/?*', function (req, res, next) {
+    if (req.params[0] === '') {
+        res.json(getRouteList());
+        return false;
+    }
+    let [api, param2, param3, param4] = req.params[0].split('/');
+    let fields;
+    let url = 'https://us.api.battle.net/wow/' + api + '/';
 
+    if (param2) {
+        url += param2;
+        if (param3) url += '/';
+    }
+    if (param3) {
+        url += param3;
+    }
 
+    url += '?';
+
+    if (api === "character") {
+        fields = 'guild,items,progression,stats,talents';
+    }
+    if (api === "guild") {
+        fields = 'members,news';
+    }
+    if (fields) {
+        if (param4) fields = param4;
+        url += 'fields=' + fields + '&';
+    }
+    url += `locale=en_US&apikey=${keys.blizzard}`;
+    fetch(url, res);
+});
+
+// diablo 3
+router.get('/api/d3/profile/:battletag/:heroid?', function (req, res, next) {
+    let hero = req.params.heroid ? '/hero/' + req.params.heroid : '';
+    let url = `https://us.api.battle.net/d3/profile/${req.params.battletag}/${hero}?locale=en_US&apikey=${keys.blizzard}`;
+    fetch(url, res);
+});
+
+function fetch(url, res) {
+    request.get(url).end((err, response) => {
+        if (response && response.text) {
+
+            let parsed = JSON.parse(response.text);
+            if (parsed.status === 'nok') {
+                res.json({
+                    error: {
+                        status: 'nok',
+                        url,
+                        reason: parsed.reason
+                    }
+                })
+            } else {
+                res.json(parsed);
+            }
+        } else {
+            console.log('Empty response text', response);
+            res.json({
+                error: {
+                    reason: 'Response.text was empty',
+                    url,
+                    response
+                }
+            })
+        }
+    });
+}
+
+function getRouteList() {
+    let validEndpoints = {
+        '/wow/achievement/:id': '/wow/achievement/2144',
+        '/wow/auction/data/:realm': '/wow/auction/data/stormreaver',
+        '/wow/boss/': '/wow/boss/',
+        '/wow/boss/:bossid': '/wow/boss/24723',
+        '/wow/challenge/:realm': '/wow/challenge/stormreaver',
+        '/wow/challenge/region': '/wow/challenge/region',
+        '/wow/character/:realm/:charactername': '/wow/character/stormreaver/lucid',
+        '/wow/data/battlegroups/': '/wow/data/battlegroups/',
+        '/wow/data/character/achievements': '/wow/data/character/achievements',
+        '/wow/data/character/classes': '/wow/data/character/classes',
+        '/wow/data/character/races': '/wow/data/character/races',
+        '/wow/data/guild/achievements': '/wow/data/guild/achievements',
+        '/wow/data/guild/perks': '/wow/data/guild/perks',
+        '/wow/data/guild/rewards': '/wow/data/guild/rewards',
+        '/wow/data/item/classes': '/wow/data/item/classes',
+        '/wow/data/pet/types': '/wow/data/pet/types',
+        '/wow/data/talents': '/wow/data/talents',
+        '/wow/guild/:realm/:guildname': '/wow/guild/stormreaver/orb',
+        '/wow/item/:itemid': '/wow/item/18803',
+        '/wow/item/set/:setid': '/wow/item/set/1060',
+        '/wow/leaderboard/:bracket': '/wow/leaderboard/2v2',
+        '/wow/mount/': '/wow/mount/',
+        '/wow/pet/': '/wow/pet/',
+        '/wow/pet/ability/:abilityid': '/wow/pet/ability/640',
+        '/wow/pet/species/:speciesid': '/wow/pet/species/258',
+        '/wow/quest/:questid': '/wow/quest/13146',
+        '/wow/realm/status': '/wow/realm/status',
+        '/wow/recipe/:recipeid': '/wow/recipe/123899',
+        '/wow/spell/:spellid': '/wow/spell/169824',
+        '/wow/zone/': '/wow/zone/',
+        '/wow/zone/:zoneid': '/wow/zone/6912'
+    };
+
+    Object.keys(validEndpoints).forEach(key => {
+        validEndpoints[key] = 'http://localhost:8080/api' + validEndpoints[key];
+    });
+
+    return {
+        validEndpoints
+    };
+}
 
 // router.get('/pano', function(req, res, next) {
 //     res.render('panorama', {});
