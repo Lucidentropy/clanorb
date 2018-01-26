b20102455f82df3a0a450cc6a5ebfbe574159918
Bryant Young <orb.lucid@gmail.com>
2018-01-25 22:12:26 -0800

    Updating steam page to show community info

--- server/router-api.js
+++ server/router-api.js
@@ -13,7 +13,7 @@ module.exports = function (router) {
 
         let recentlyPlayedGamesUrl = 'http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=' + steamK + '&steamid={STEAMID}&format=json';
         let ownedGamesUrl = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + steamK + '&steamid={STEAMID}&format=json';
-        
+
         let groupMemberListUrl = 'http://steamcommunity.com/groups/orb/memberslistxml/';
         let playerSummaryUrl = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + steamK + '&steamids={STEAMIDS}&format=json';
 
@@ -34,12 +34,31 @@ module.exports = function (router) {
                             return nameA > nameB ? 1 :
                                 nameA < nameB ? -1 : 0;
                         });
-                        res.json(result.memberList);
+
+                        // cleanup
+                        let x = result.memberList;
+                        res.json({
+                            groupID64: x.groupID64[0],
+                            groupName: x.groupDetails[0].groupName[0],
+                            groupURL: x.groupDetails[0].groupURL[0],
+                            headline: x.groupDetails[0].headline[0],
+                            summary: x.groupDetails[0].summary[0],
+                            avatarIcon: x.groupDetails[0].avatarIcon[0],
+                            avatarMedium: x.groupDetails[0].avatarMedium[0],
+                            avatarFull: x.groupDetails[0].avatarFull[0],
+                            membersInChat: parseInt(x.groupDetails[0].membersInChat[0]),
+                            membersInGame: parseInt(x.groupDetails[0].membersInGame[0]),
+                            membersOnline: parseInt(x.groupDetails[0].membersOnline[0]),
+                            memberCount: parseInt(x.memberCount[0]),
+                            members: result.memberList.members
+                        });
                     });
                 });
             });
         } else {
-            res.json({error : "No section provided" })
+            res.json({
+                error: "No section provided"
+            })
         }
     });
 
